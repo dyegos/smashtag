24df8e8780b7fa9d4decf8c206270ee706feef87
Dyego Silva <dyego.j.silva@gmail.com>
2015-12-23 22:08:17 -0200

    implementando a tela que mostra o tweet...marcado as hashtags, os links e os mentions. Falta mostrar a media, se houver

--- smashtag/TweetViewController.swift
+++ smashtag/TweetViewController.swift
@@ -10,18 +10,69 @@ import UIKit
 
 class TweetViewController: UIViewController
 {
-    var tweet: Tweet? { didSet { print(tweet) } }
-
-    @IBOutlet weak var tweetText: UITextView! { didSet { updateUI() } }
+    var tweet: Tweet?
+    
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userhastag: UILabel!
+    @IBOutlet weak var userTweet: UILabel!
+    @IBOutlet weak var media: UIImageView!
     
     func updateUI()
     {
-        tweetText?.text = nil
+        guard let tweet = self.tweet else { return }
         
-        if let tweet = self.tweet
+        if let profileImage = tweet.user.profileImageURL
         {
-            tweetText?.text = tweet.text
+            dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0))
+            {
+                if let imageData = NSData(contentsOfURL: profileImage)
+                {
+                    dispatch_async(dispatch_get_main_queue())
+                    {
+                        self.userImage?.image = UIImage(data: imageData)
+                    }
+                }
+            }
         }
+        
+        let attribute: NSMutableAttributedString = NSMutableAttributedString(string: tweet.text)
+        
+        for url in tweet.urls
+        {
+            attribute.addAttributes(
+                [NSForegroundColorAttributeName : UIColor.blueColor()/*,
+                    NSLinkAttributeName : url.keyword,
+                    NSUnderlineStyleAttributeName: NSUnderlineStyle.StyleNone.rawValue*/], range: url.nsrange)
+        }
+        
+        for hashtags in tweet.hashtags
+        {
+            attribute.addAttributes(
+                [NSForegroundColorAttributeName : UIColor.blueColor()/*,
+                    /*NSLinkAttributeName : hashtags.keyword,*/
+                    NSUnderlineStyleAttributeName: NSUnderlineStyle.PatternSolid.rawValue*/], range: hashtags.nsrange)
+        }
+        
+        for mentions in tweet.userMentions
+        {
+            attribute.addAttributes(
+                [NSForegroundColorAttributeName : UIColor.blueColor()/*,
+                NSLinkAttributeName : mentions.keyword,
+                NSUnderlineStyleAttributeName: NSUnderlineStyle.StyleNone.rawValue*/], range: mentions.nsrange)
+        }
+        
+        userName?.text = tweet.user.name
+        userhastag?.text = "@" + tweet.user.screenName
+        userTweet?.attributedText = attribute
+
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        updateUI()
     }
     
 
